212f015fad721e2883087dbbfcf5766cab10eedf
Matthew Beckler <matthew.beckler@gmail.com>
2015-07-12 15:52:44 -0500

    Added CC0 public domain license to all new bricktronics example code

--- examples/LightSensor/LightSensor.ino
+++ examples/LightSensor/LightSensor.ino
@@ -13,6 +13,13 @@
 // We also flash the Light Sensor's LED between each sample. We've found
 // that the light doesn't help much in distinguishing between light and dark
 // surfaces, but your mileage may vary, so give it a try.
+//
+// Written in 2015 by Matthew Beckler and Adam Wolf for Wayne and Layne, LLC
+// To the extent possible under law, the author(s) have dedicated all
+//   copyright and related and neighboring rights to this software to the
+//   public domain worldwide. This software is distributed without any warranty.
+// You should have received a copy of the CC0 Public Domain Dedication along
+//   with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>. 
 
 
 // Include the Bricktronics Light sensor library
